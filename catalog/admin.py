ad60b9889313dad5a2891a92a3c490fe612d46cb
nouh ben kadi <benkadi.nouh@icloud.com>
2020-10-07 17:53:37 +0100

    added inlineInstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+from catalog.models import Genre, Language, Author, Book, BookInstance
+admin.site.register(Genre)
+#admin.site.register(Author)
+#admin.site.register(Book)
+admin.site.register(Language)
+#admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name','date_of_birth','date_of_death')
+    #using ('date_of_birth', 'date_of_death') will display these fields horizontally
+    fields = ['name', ('date_of_birth', 'date_of_death')]
+    
+#register the model with its corresponding new admin view instead of the default view
+admin.site.register(Author, AuthorAdmin)
+
+#This class is to get info about instances inside the detail view of the model book
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+#Register the Admin classes for BookInstance using the decorator
+#the @register decorator to register the models 
+#(this does exactly the same thing as the admin.site.register(BookInstance)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book','loan_status','due_back_date',)
+    list_filter = ('book','loan_status','due_back_date')
+    #To section the detail view of bookInstance into groups: the info related to someting put theme in a 
+    #section together
+    fieldsets = (
+        ('Instance Info',{
+            'fields':('book','imprint','id'),
+        }),
+        ('Availability',{
+            'fields':('loan_status','due_back_date')
+        }),
+    )
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,72 @@
 from django.db import models
-
+from django.urls import reverse
+import uuid
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(auto_now=False,auto_now_add=False,null=True, blank=True)
+    date_of_death = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True, verbose_name='Died')
+    class Meta:
+        ordering = ['name']
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100,help_text='Enter a book genre (e.g. Science Fiction)')
+    def __str__(self):
+        return self.name
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    #Because some authors my not be declared yet
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN',max_length=13)
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail',args=[str(self.id)])
+
+    def display_genre(self):
+        """Creates a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genrex' #This is used like verbose_name for the function display_genre
+    #when it is called from a view 'display_genre' just as we call a normal field
+
+    def display_language(self):
+        """Creates a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join([language.name for language in self.language.all()[:3]])
+    display_language.short_description = 'Language'
+
+class BookInstance(models.Model):
+    LOAN_STATUS = [
+        ('M', 'Maintenance'),
+        ('O', 'On loan'),
+        ('A', 'Available'),
+        ('R', 'Reserved'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,help_text='Unique ID for this particular book across whole library')
+    due_back_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True, verbose_name='Due Back')
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    loan_status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability',)
+    imprint = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['-due_back_date']
+
+    def __str__(self):
+        return f'{self.id} {self.book.title}'
+
